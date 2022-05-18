31e3c922fffa1d2975b1dace4b34ef28a27bc7ac
azgarian <cemazgari@gmail.com>
2022-05-18 08:29:11 +0000

    simulation works with ron file

--- workflow/scripts/simulation.sh
+++ workflow/scripts/simulation.sh
@@ -2,18 +2,23 @@
 
 set -e 
 
-(echo "`date -R`: Converting {input.bed} to fasta format..." &&
+( echo "`date -R`: Converting $1 to fasta format..." &&
 bedtools getfasta \
 -fi $2 \
 -bed $1 \
 -fo $1.fa \
 -s &&
-echo "`date -R`: Success! {input.bed} is converted." || 
-{{ echo "`date -R`: Process failed..."; exit 1; }}  ) > $7 2>&1
+echo "`date -R`: Success! $1 is converted." || 
+{ echo "`date -R`: Process failed..."; exit 1; }  ) > $7 2>&1
 
-if [ "$3" == *.ron ]; then
+#echo "$3"  >> $7 2>&1
 
-    (echo "`date -R`: Simulating reads according to reference genome..." &&
+if [[ "$3" == *.ron ]];
+then
+
+    #echo "Ron" 
+
+    ( echo "`date -R`: Simulating reads according to reference genome..." &&
     boquila \
     --fasta $1.fa \
     --bed $5 \
@@ -22,13 +27,14 @@ if [ "$3" == *.ron ]; then
     --sens 2 \
     --regions $3 \
     > $6 &&
-    {{ echo "`date -R`: Success! Simulation is done."; rm $1.fa }} || 
-    {{ echo "`date -R`: Process failed..."; rm $5; rm $6; exit 1; }} \
-    ) >> $7 2>&1
+    { echo "`date -R`: Success! Simulation is done."; rm $1.fa; } || 
+    { echo "`date -R`: Process failed..."; rm $5; rm $6; exit 1; } ) >> $7 2>&1
 
 else
 
-    (echo "`date -R`: Simulating reads with input file..." &&
+    #echo "Input" 
+
+    ( echo "`date -R`: Simulating reads with input file..." &&
     boquila \
     --fasta $1.fa \
     --inseqFasta \
@@ -36,36 +42,36 @@ else
     --seed 1 \
     --sens 2 \
     > $6 &&
-    {{ echo "`date -R`: Success! Simulation is done."; rm $1.fa }}  || 
-    {{ echo "`date -R`: Process failed..."; rm $6; exit 1; }}  ) >> $7 2>&1
+    { echo "`date -R`: Success! Simulation is done."; rm $1.fa; }  || 
+    { echo "`date -R`: Process failed..."; rm $6; exit 1; }  ) >> $7 2>&1
 
-    (echo "`date -R`: Aligning fasta file..." &&
+    ( echo "`date -R`: Aligning fasta file..." &&
     bowtie2 \
     -x $4 \
     -f $6 -S $6.sam &&
     echo "`date -R`: Success! Alignment is done." || 
-    {{ echo "`date -R`: Process failed..."; exit 1; }}  ) >> $7 2>&1
+    { echo "`date -R`: Process failed..."; exit 1; }  ) >> $7 2>&1
 
-    (echo "`date -R`: Converting sam to bam..." &&
+    ( echo "`date -R`: Converting sam to bam..." &&
     samtools view -Sbh -o $6.bam $6.sam &&
-    {{ echo "`date -R`: Success! Conversion is done."; rm $6.sam }} || 
-    {{ echo "`date -R`: Process failed..."; exit 1; }}  ) >> $7 2>&1
+    { echo "`date -R`: Success! Conversion is done."; rm $6.sam; } || 
+    { echo "`date -R`: Process failed..."; exit 1; }  ) >> $7 2>&1
 
-    (echo "`date -R`: Sorting (coordinates) bam file..." &&
+    ( echo "`date -R`: Sorting (coordinates) bam file..." &&
     samtools sort $6.bam > $6_sorted.bam &&
-    {{ echo "`date -R`: Success! Bam file is sorted."; rm $6.bam }} || 
-    {{ echo "`date -R`: Process failed..."; exit 1; }}  ) >> $7 2>&1
+    { echo "`date -R`: Success! Bam file is sorted."; rm $6.bam; } || 
+    { echo "`date -R`: Process failed..."; exit 1; }  ) >> $7 2>&1
 
-    (echo "`date -R`: Index bam file..." &&
+    ( echo "`date -R`: Index bam file..." &&
     samtools index $6_sorted.bam $6_sorted.bam.bai &&
     echo "`date -R`: Success! Bam file is sorted." || 
-    {{ echo "`date -R`: Process failed..."; exit 1; }}  ) >> $7 2>&1
+    { echo "`date -R`: Process failed..."; exit 1; }  ) >> $7 2>&1
 
-    (echo "`date -R`: Processing bam file..." && 
+    ( echo "`date -R`: Processing bam file..." && 
     samtools view -b $6_sorted.bam |&
     bedtools bamtobed > $5 &&
-    {{echo "`date -R`: Success! Bam file converted to bed format."; \
-    rm $6_sorted.bam; rm $6_sorted.bam.bai }} || 
-    {{ echo "`date -R`: Process failed..."; rm $5; exit 1; }}  ) >> $7 2>&1
+    { echo "`date -R`: Success! Bam file converted to bed format."; \
+    rm $6_sorted.bam; rm $6_sorted.bam.bai; } || 
+    { echo "`date -R`: Process failed..."; rm $5; exit 1; }  ) >> $7 2>&1
 
 fi
