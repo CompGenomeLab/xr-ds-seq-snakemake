4d7900d567d322a8ae3d77be7bac73715cff46cb
azgarian <cemazgari@gmail.com>
2025-08-13 11:22:32 +0300

    test: begin adding unit tests for rules

---
+++ .tests/unit/test_sra_se.py
@@ -0,0 +1,45 @@
+import os
+import sys
+
+import subprocess as sp
+from tempfile import TemporaryDirectory
+import shutil
+from pathlib import Path, PurePosixPath
+
+sys.path.insert(0, os.path.dirname(__file__))
+
+import common
+
+
+def test_sra_se():
+
+    with TemporaryDirectory() as tmpdir:
+        workdir = Path(tmpdir) / "workdir"
+        data_path = PurePosixPath(".tests/unit/sra_se/data")
+        expected_path = PurePosixPath(".tests/unit/sra_se/expected")
+
+        # Copy data to the temporary workdir.
+        shutil.copytree(data_path, workdir)
+
+        # dbg
+        print("resources/samples/test_xr_single_cpd_sim_input_paired.fastq.gz", file=sys.stderr)
+
+        # Run the test job.
+        sp.check_output([
+            "python",
+            "-m",
+            "snakemake", 
+            "resources/samples/test_xr_single_cpd_sim_input_paired.fastq.gz",
+            "-f", 
+            "-j1",
+            "--target-files-omit-workdir-adjustment",
+    
+            "--directory",
+            workdir,
+        ])
+
+        # Check the output byte by byte using cmp.
+        # To modify this behavior, you can inherit from common.OutputChecker in here
+        # and overwrite the method `compare_files(generated_file, expected_file), 
+        # also see common.py.
+        common.OutputChecker(data_path, expected_path, workdir).check()
